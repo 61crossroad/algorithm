82fb1d456222f79c1304e4488a5a76e9237582e0
Kim Seunghwan <61crossroad@gmail.com>
2021-02-19 00:21:56 +0900

    very simple recursive binary search

---
+++ BinarySearchRecursive.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    private static int location(int low, int high, int x, List<Integer> list) {
+        int mid;
+
+        if (low > high) {
+            return 0;
+        } else {
+            mid = (int) Math.floor((low + high) / 2);
+
+            if (list.get(mid) == x) {
+                return mid;
+            } else if (x < list.get(mid)) {
+                return location(low, mid - 1, x, list);
+            } else {
+                return location(mid + 1, high, x, list);
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("BinarySearchRecursive");
+
+        List<Integer> list = Arrays.asList(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17, 20});
+        int sol = location(0, list.size(), 11, list);
+        System.out.println("sol = " + sol);
+    }
+}
