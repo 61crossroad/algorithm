247ad35f5a5764e07acdba328d60696f69e361b9
iMac21.5-2012 <61crossroad@gmail.com>
2021-06-14 01:32:21 +0900

    assign, list reverse, list slice

---
+++ PySolver/reverse_string.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def reverse_list(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+        print(s)
+
+    def reverse(self, s: List[str]) -> None:
+        s.reverse()
+        print(s)
+
+    def reverse_string(self, s: str) -> None:
+        s = s[::-1]
+        print(s)
+
+
+if __name__ == '__main__':
+    # Solution.reverse_list(["h", "e", "l", "l", "o"])
+    Solution.reverse(["h", "e", "l", "l", "o"])
+    Solution.reverse_string('nooyij')
