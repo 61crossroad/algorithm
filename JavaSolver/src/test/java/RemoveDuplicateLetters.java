44226ef0d12eb24ee2a443ad68b9191a39982a63
iMac21.5-2012 <61crossroad@gmail.com>
2021-06-14 02:42:43 +0900

    Divide & Conquer, TreeSet, "" is a valid String

--- JavaSolver/src/test/java/RemoveDuplicateLetters.java
+++ JavaSolver/src/test/java/RemoveDuplicateLetters.java
@@ -4,6 +4,11 @@ import java.util.TreeSet;
 
 import org.junit.jupiter.api.Test;
 
+/*
+Array -> List -> Set (134ms)
+Array -> Set (127ms)
+ */
+
 public class RemoveDuplicateLetters {
     
     @Test
@@ -18,9 +23,24 @@ public class RemoveDuplicateLetters {
         }
 
         Set<String> set = new TreeSet<>(Arrays.asList(s.split("")));
+        /*
+        Set<String> set = new TreeSet<>();
+        Set<String> suffix = new TreeSet<>();
+        for (String ch : s.split("")) {
+            set.add(ch);
+        }
+        */
+
         for (String ch : set) {
             String sub = s.substring(s.indexOf(ch));
             Set<String> suffix = new TreeSet<>(Arrays.asList(sub.split("")));
+            /*
+            suffix.clear();
+            for (String subCh : sub.split("")) {
+                suffix.add(subCh);
+            }
+            */
+
             if (suffix.equals(set)) {
                 return ch + removeDuplicateLetters(sub.replaceAll(ch, ""));
             }
