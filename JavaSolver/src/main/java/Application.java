ca75005e4ea4c07aac9e8dd574ce97dff1eb13c9
iMac21.5-2012 <61crossroad@gmail.com>
2021-06-13 16:56:18 +0900

    JUnit

--- JavaSolver/src/main/java/Application.java
+++ JavaSolver/src/main/java/Application.java
@@ -2,7 +2,7 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println(
-                "Every solution is implemented with jUnit Test\n" +
+                "Every solution is implemented with JUnit Test\n" +
                 "So, look at \"/src/test/java\"");
     }
 }
