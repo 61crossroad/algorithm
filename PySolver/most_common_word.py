cab9f8c80981c8bff727b9d18fb2f4e185aea44c
iMac21.5-2012 <61crossroad@gmail.com>
2021-06-14 01:31:29 +0900

    re.sub, split, collections

--- PySolver/most_common_word.py
+++ PySolver/most_common_word.py
@@ -3,23 +3,22 @@ import re
 from typing import List
 
 
-def most_common_word(paragraph: str, banned: List[str]):
-    words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
-        .lower().split()
-             if word not in banned]
+class Solution:
+    def most_common_word(self, paragraph: str, banned: List[str]):
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
 
-    # counts = collections.defaultdict(int)
-    # for word in words:
-    #     counts[word] += 1
-    #
-    # return max(counts, key=counts.get)
+        # counts = collections.defaultdict(int)
+        # for word in words:
+        #     counts[word] += 1
+        #
+        # return max(counts, key=counts.get)
 
-    counts = collections.Counter(words)
-    return counts.most_common(1)[0][0]
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]
 
 
 if __name__ == '__main__':
     paragraph = 'Bob hit a ball, the hit BALL flew far after it was hit.'
     banned = ['hit']
 
-    print(most_common_word(paragraph, banned))
+    print(Solution.most_common_word(paragraph, banned))
