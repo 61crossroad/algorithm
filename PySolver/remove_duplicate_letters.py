4a5d989a1de95c25a17374c282669b08249e4cc6
iMac21.5-2012 <61crossroad@gmail.com>
2021-06-14 02:33:45 +0900

    sorted(set(s))

--- PySolver/remove_duplicate_letters.py
+++ PySolver/remove_duplicate_letters.py
@@ -1,11 +1,12 @@
-class solution:
+class Solution:
     def recursion(self, s: str) -> str:
         for char in sorted(set(s)):
             suffix = s[s.index(char):]
             if set(s) == set(suffix):
                 return char + self.recursion(suffix.replace(char, ''))
+        return ''
 
 
 if __name__ == '__main__':
     s = 'cbacdcbc'
-    solution.recursion(s)
+    print(Solution().recursion(s))
