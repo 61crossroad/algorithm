a1b7f1586e2767ead2fb0cfca683ac71ff4ba7d0
iMac21.5-2012 <61crossroad@gmail.com>
2021-06-27 17:20:40 +0900

    dfs, deepcopy with stream & Collectors

---
+++ JavaSolver/src/test/java/Permutations.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+public class Permutations {
+    private List<List<Integer>> results = new ArrayList<>();
+    
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> elements = Arrays.stream(nums).boxed().collect(Collectors.toList());
+
+        dfs(elements, new ArrayList<>());
+
+        return results;
+    }
+
+    private void dfs(List<Integer> elements, List<Integer> perm) {
+        if (elements == null || elements.isEmpty()) {
+            results.add(
+                perm.stream()
+                    .collect(Collectors.toList()));
+        } else {
+            for (Integer e : elements) {
+                List<Integer> nextElements = 
+                    elements.stream()
+                        .filter(el -> !el.equals(e))
+                        .collect(Collectors.toList());
+                
+                perm.add(e);
+                dfs(nextElements, perm);
+                perm.remove(e);
+            }
+        }
+    }
+    
+    @Test
+    public void run() {
+        int[] nums = new int[] {1, 2, 3};
+        List<List<Integer>> output = permute(nums);
+        output.forEach(perm -> {
+            System.out.println(
+                perm.stream()
+                    .map(num -> String.valueOf(num))
+                    .collect(Collectors.joining(" ")));
+        });
+    }
+}
