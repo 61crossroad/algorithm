dc668d8f5e6a8292bed98b61b4b93ff49d50bb9a
김승환 at Home <61crossroad@gmail.com>
2022-01-09 23:39:58 +0900

    sliding window

--- JavaSolver/src/test/java/LongestRepeatingCharacterReplacement.java
+++ JavaSolver/src/test/java/LongestRepeatingCharacterReplacement.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class LongestRepeatingCharacterReplacement {
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestRepeatingCharacterReplacement {
+    @Test
+    public void run() {
+        System.out.println(characterReplacement("AABABBA", 1));
+    }
+
+    private int characterReplacement(String s, int k) {
+        String[] sArr = s.split("");
+        Map<String, Integer> counts = new HashMap<>();
+        int left, right;
+        left = right =0;
+
+        for (; right < sArr.length; right++) {
+            counts.put(sArr[right], counts.getOrDefault(sArr[right], 0) + 1);
+            int max = 0;
+
+            for (Integer count : counts.values()) {
+                max = Math.max(max, count);
+            }
+
+            if (right - left + 1 - max > k) {
+                counts.put(sArr[left], counts.get(sArr[left]) - 1);
+                left++;
+            }
+        }
+
+        return right - left;
+    }
 }
--- JavaSolver/src/test/java/MinimumWindowSubstringAlt.java
+++ JavaSolver/src/test/java/MinimumWindowSubstringAlt.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class MinimumWindowSubstringAlt {
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinimumWindowSubstringAlt {
+
+    @Test
+    public void run() {
+        System.out.println(minWindow("a", "aa"));
+    }
+
+    private String minWindow(String s, String t) {
+        Map<String, Integer> counts = new HashMap<>();
+        String[] sArr = s.split("");
+        String[] tArr = t.split("");
+        int missing = tArr.length, min = sArr.length + 1;
+        String answer = "";
+
+        for (String str : tArr) {
+            counts.put(str, counts.getOrDefault(str, 0) + 1);
+        }
+        for (int count, left = 0, right = 0; right < sArr.length; right++) {
+            count = counts.getOrDefault(sArr[right], 0);
+            missing = count > 0 ? missing - 1 : missing;
+            counts.put(sArr[right], count - 1);
+
+            if (missing == 0) {
+                while ((count = counts.get(sArr[left])) < 0) {
+                    counts.put(sArr[left], count + 1);
+                    left++;
+                }
+
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
+                    answer = s.substring(left, right + 1);
+                }
+
+                counts.put(sArr[left], counts.get(sArr[left]) + 1);
+                left++;
+                missing++;
+            }
+        }
+
+        return answer;
+    }
 }
